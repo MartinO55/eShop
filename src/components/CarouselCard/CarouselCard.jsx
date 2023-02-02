d429b98262d688d0e9bdb3923372bb3c6471c0e9
Martin Olson <martinguyolson@gmail.com>
2023-02-02 13:35:02 +1100

    created components for the carousel

---
+++ src/components/CarouselCard/CarouselCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./CarouselCard.module.scss";
+
+const CarouselCard = () => {
+  return <div>CarouselCard</div>;
+};
+
+export default CarouselCard;
---
+++ src/components/CarouselContainer/CarouselContainer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./CarouselContainer.module.scss";
+
+const CarouselContainer = () => {
+  return <div>CarouselContainer</div>;
+};
+
+export default CarouselContainer;
