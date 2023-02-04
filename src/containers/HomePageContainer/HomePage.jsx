205851cf136098fba1143a000c5b2bcda269d724
Martin Olson <martinguyolson@gmail.com>
2023-02-04 13:54:50 +1100

    created product gallery page. still unsure where it should be

--- src/containers/HomePageContainer/HomePage.jsx
+++ src/containers/HomePageContainer/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import CarouselOverflowContainer from "../CarouselOverFlowContainer/CarouselOverflowContainer";
+import ProductGallery from "../../components/ProductGallery/ProductGallery";
 
 const HomePage = () => {
   return (
     <>
       <CarouselOverflowContainer />
+
+      {/* So I think we need a grid of all the products here  - clicking one should take you to the product page for that specific product*/}
+      <ProductGallery />
     </>
   );
 };
