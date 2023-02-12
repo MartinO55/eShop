12843e7e214e587e3c1793ccbdc0229458b4fabe
Martin Olson <martinguyolson@gmail.com>
2023-02-13 09:13:09 +1100

    polish 404

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "./NotFound.module.scss";
+import fourohfour from "../../assets/404Cat.jpg";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <>
+      <div className={styles.NotFound}>
+        <img src={fourohfour} alt="404" className={styles.fourohfour} />
+      </div>
+    </>
+  );
 };
 
 export default NotFound;
